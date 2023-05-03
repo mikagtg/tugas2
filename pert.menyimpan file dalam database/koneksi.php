12824fa417e7ccbc056408fc10a8ad8b52a2e599
mikagtg <130830462+mikagtg@users.noreply.github.com>
2023-05-03 13:22:37 +0700

    Add files via upload

---
+++ pert.menyimpan file dalam database/koneksi.php
@@ -0,0 +1,18 @@
+<?php
+$host = "localhost";
+$user= "root";
+$pass = "";
+$db = "db_latihan";
+
+$koneksi = mysqli_connect ($host, $user, $pass, $db);
+
+//test koneksi
+
+if ($koneksi){
+    echo " Koneksi Berhasil" , "<br>";
+
+}
+else {
+    echo " Koneksi Gagal";
+}
+?>
